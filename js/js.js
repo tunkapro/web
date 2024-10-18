b668280c22a26b4ddf1bc559de2640c3aeebf828
tunkapro <truongtuan20042004@gmail.com>
2024-10-18 12:26:44 +0700

    js

--- js/js.js
+++ js/js.js
@@ -1,5 +1,25 @@
 /*Name this external file gallery.js*/
 
+function getImage(){
+   const elements = document.getElementsByClassName('preview');
+   for(let i = 0; i < elements.length; i++){
+      elements[i].setAttribute("tabindex", "1");
+   }
+   for(let i = 0; i<= elements.length; i++){
+      if(i == elements.length){
+         document.getElementById('image').setAttribute("tabindex", "0");
+         document.getElementById('image').addEventListener("focus", function(){
+         document.getElementById('image').style.backgroundImage = null;
+         document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+         })
+         break;
+      }
+      elements[i].addEventListener("focus", function(){
+         document.getElementById('image').style.backgroundImage = `url(${this.src})`;
+         document.getElementById('image').innerHTML = this.alt;
+      })
+   }
+}
 function upDate(previewPic){
     /* In this function you should 
        1) change the url for the background image of the div with the id = "image" 
@@ -12,6 +32,7 @@ function upDate(previewPic){
           document.getElementById('image').style.backgroundImage = `url(${urlImage})`;
           let altText = previewPic.alt;
           document.getElementById('image').innerHTML = altText;
+
        }
        function unDo(){
         /* In this function you should 
